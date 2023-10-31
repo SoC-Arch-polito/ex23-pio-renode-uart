2da64c4776e2697c630bae18987566485359698a
Walter Gallego <waltergallegog@gmail.com>
2023-10-31 16:25:02 +0100

    Fix indentation

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-
 #include <zephyr/kernel.h>
 #include <string.h>
 #include <zephyr/sys/printk.h>
@@ -9,14 +8,14 @@ void main(void)
     printk("Hello! I'm running on the %s, a %s board.\n\n ",
            CONFIG_BOARD, CONFIG_ARCH);
 
-	console_getline_init();
-	printk("Enter a line finishing with Enter:\n");
+    console_getline_init();
+    printk("Enter a line finishing with Enter:\n");
 
-	while (1)
+    while (1)
     {
         printk("> ");
-		char *s = console_getline();
-		printk("Typed line: %s\n", s);
-		printk("Last char was: 0x%x\n", s[strlen(s) - 1]);
-	}
+        char *s = console_getline();
+        printk("Typed line: %s\n", s);
+        printk("Last char was: 0x%x\n", s[strlen(s) - 1]);
+    }
 }
