2588289bdb17b5ec8c32448c2ec4b8191c99d387
Walter Gallego <waltergallegog@gmail.com>
2023-10-31 16:19:56 +0100

    Upgrade to zephyr version 3.4
    Update zephyr sources to support version 3.4.
    
    Changes based on
    https://github.com/zephyrproject-rtos/zephyr/tree/zephyr-v3.4.0/samples/subsys/console/getline

--- src/main.c
+++ src/main.c
@@ -1,22 +1,22 @@
-#include <zephyr.h>
-#include <version.h>
+
+#include <zephyr/kernel.h>
 #include <string.h>
-#include <sys/printk.h>
-#include <console/console.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/console/console.h>
 
 void main(void)
 {
-   printk("Hello! I'm running Zephyr %s on %s, a %s board.\n\n ",
-           KERNEL_VERSION_STRING, CONFIG_BOARD, CONFIG_ARCH);
+    printk("Hello! I'm running on the %s, a %s board.\n\n ",
+           CONFIG_BOARD, CONFIG_ARCH);
 
-   console_getline_init();
-   printk("Enter a line finishing with Enter:\n");
+	console_getline_init();
+	printk("Enter a line finishing with Enter:\n");
 
-   while (1)
-   {
-       printk("> ");
-       char *s = console_getline();
-       printk("Typed line: %s\n", s);
-       printk("Last char was: 0x%x\n", s[strlen(s) - 1]);
-   }
+	while (1)
+    {
+        printk("> ");
+		char *s = console_getline();
+		printk("Typed line: %s\n", s);
+		printk("Last char was: 0x%x\n", s[strlen(s) - 1]);
+	}
 }
